684ad27835f2c724ec831cca1fd04de19ad90272
ursala lindsey <ursalalindsey@ursalas-MacBook-Air.local>
2020-11-03 19:03:03 -0500

    Trying to add for loop  to hit test 8 and 9

--- orderBook.js
+++ orderBook.js
@@ -1,59 +1,53 @@
-const  reconcileOrder  = (existingBook, incomingOrder) => {
-  // new func to balance book
-  let balanceBook = (existingBook) => {
-    existingBook.map((order) => {
-      // console.log(existingBook)
-      if (existingBook[0].type !== order.type && existingBook[0].price === order.price){
-        // remove the found matching order
-        existingBook.splice(0, 2)
+const reconcileOrder = (ex, incoming) => {
+  // let balanceBook = (existingBook, index) => {
+  //   existingBook.map((order) => {
+  //     // console.log(existingBook)
+  //     if (existingBook[0].type !== order.type && existingBook[0].price === order.price){
+  //       // remove the found matching order
+  //       existingBook.splice(index, existingBook.length-1)
         
-      } 
-      return existingBook
+  //     } 
+  //     return existingBook
       
-    })
+  //   })
 
+  // }
+  console.log(ex)
+  console.log('++++++++++++')
+  console.log(incoming)
+ 
+  if(ex.length === 0){
+    ex.push(incoming)
+    return ex
   }
+  // const sameIndex = ex.findIndex((order) => { // only find incoming price === existing price  && incoming type !== existing type
+  //   return incoming.price === order.price && incoming.type !== order.type
+  // })
+
+  for(let i = 0; i < ex.length; i++) {
+    if (ex[i].price !== incoming.price || ex[i].type === incoming.type)  {
+      ex.push(incoming)
+      return ex
+    } else if(ex[i].quantity === incoming.quantity && ex[i].type !== incoming.price){
+      ex.splice(ex[i].length, 1)
+    }
+    
+     
+      
+    }
+    return ex
+    
+    // if(ex.quantity[0] > incoming.quantity) {
+    //   ex.quantity = ex.quantity -incoming.quantity
+
+    // }
   
+  }
 
-  const sameIndex = existingBook.findIndex((order) => { // only find incoming price === existing price  && incoming type !== existing type
-    return incomingOrder.price === order.price && incomingOrder.type !== order.type
-  })
-
-
-
-
-  if (sameIndex === -1) { // none meet the above conditions -- 1,2, 3
-    existingBook.push(incomingOrder)
-    return existingBook 
-  } else if (incomingOrder.quantity === existingBook[sameIndex].quantity){ // quantity is the same 4
-    existingBook.splice(sameIndex, 1)        
-    return existingBook 
-  } else if (existingBook[sameIndex].quantity > incomingOrder.quantity){
-    existingBook[sameIndex].quantity = existingBook[sameIndex].quantity - incomingOrder.quantity 
-    
-    // console.log(existingBook)
-    return existingBook.reverse()
-        
-  } else if (existingBook[sameIndex].quantity < incomingOrder.quantity) {
-   
-    existingBook[sameIndex].quantity = (incomingOrder.quantity - existingBook[sameIndex].quantity) 
-    existingBook[sameIndex].type = 'sell'
-    balanceBook(existingBook)
-    return existingBook.reverse()
-  } 
-    
 
 
 }
 
-
-    
- 
-
- 
-
-  
-  
 
 
 
